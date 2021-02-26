74ddc12bf0f1a11f5286b8bcafef75a1267ba651
hhg8840 <getup9@gmail.com>
2021-02-27 08:04:59 +0900

    20210227 27

--- accountApp/views.py
+++ accountApp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 # Create your views here.
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
@@ -12,22 +12,28 @@ from accountApp.models import HelloWorld
 
 
 def hello_world(request):
-    if request.method == "POST":
 
-        temp = request.POST.get('hello_world_input')
+    if request.user.is_authenticated:
+        if request.method == "POST":
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
+            temp = request.POST.get('hello_world_input')
 
-        hello_world_list = HelloWorld.objects.all()
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
 
-        return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
-        #return HttpResponseRedirect(reverse('accountApp:hello_world'))
+            hello_world_list = HelloWorld.objects.all()
+
+            return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
+            #return HttpResponseRedirect(reverse('accountApp:hello_world'))
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
+            # hello_world_list = HelloWorld.objects.all()
     else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
-        # hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountApp:login'))
+
+
 
 class AccountCreateView(CreateView):
     model = User
@@ -42,12 +48,37 @@ class AccountDetailView(DetailView):
 
 class AccountUpdateView(UpdateView):
     model = User
+    context_object_name = 'target_user'
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountApp:hello_world')
     template_name = 'accountApp/update.html'
 
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
 class AccountDeleteView(DeleteView):
     model = User
-    # form_class = AccountUpdateForm
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountApp:login')
     template_name = 'accountApp/delete.html'
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and  self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
