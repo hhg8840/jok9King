2eea4d0c669fa0f7f7215d16323e3324882bfe1d
hhg8840 <getup9@gmail.com>
2021-03-03 06:54:07 +0900

    20210303 32

--- profileApp/views.py
+++ profileApp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileApp.decorators import profile_ownership_required
 from profileApp.forms import ProfileCreationForm
 from profileApp.models import Profile
 
@@ -23,3 +25,15 @@ class ProfileCreateView(CreateView):
 
     # def get_success_url(self):
     #     return reverse('accountApp:detail', kwargs={'pk': self.object.user.pk})
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountApp:hello_world')
+    template_name = 'profileApp/update.html'
+
+    # def get_success_url(self):
+        # return reverse('accountApp:detail', kwargs={'pk': self.object.user.pk})
