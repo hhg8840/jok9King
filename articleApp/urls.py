b572b6b15c7ac47d7715b54ba59901496b9d4a6a
getup9@gmail.com <gksgmdrn!234>
2021-03-04 18:22:00 +0900

    210304 35 course

--- articleApp/urls.py
+++ articleApp/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-# from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView, ArticleListView
+from articleApp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
 
 app_name = 'articleApp'
 
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleApp/list.html'), name='list'),
-    # path('create/', ArticleCreateView.as_view(), name='create'),
-    # path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
-    # path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
-    # path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
+    path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
 ]
--- articleApp/views.py
+++ articleApp/views.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView, DetailView, DeleteView, UpdateView
+
+from articleApp.decorators import article_ownership_required
+from articleApp.forms import ArticleCreationForm
+from articleApp.models import Article
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    template_name = 'articleApp/create.html'
+
+    def form_valid(self, form):
+        temp_article = form.save(commit=False)
+        temp_article.writer = self.request.user
+        temp_article.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleApp:detail', kwargs={'pk': self.object.pk})
+
+class ArticleDetailView(DetailView):
+    model = Article
+    # form_class = CommentCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleApp/detail.html'
+
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleApp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleApp:detail', kwargs={'pk': self.object.pk})
+
+
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    success_url = reverse_lazy('articleApp:list')
+    template_name = 'articleApp/delete.html'
